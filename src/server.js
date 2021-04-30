3414ba3538291d54f213635811b400d942343197
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2021-04-30 10:55:11 -0700

    better way of managing the files. create them from hash in public directory and redirect to download

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const GIFEncoder = require('gifencoder');
 const express = require('express');
 const { createCanvas } = require('canvas');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const tmp = require('tmp');
 const app = express();
 const port = 6969;
+const fs = require('fs');
 
-const encoder = new GIFEncoder(34, 34);
 
 app.use(bodyParser.json({limit: '20mb'}));
 
@@ -14,22 +16,35 @@ app.use('/token-maker', express.static('public'));
 
 
 const saveGif = (request, response) => {
+  const encoder = new GIFEncoder(34, 34);
   const frames = request.body;
-  encoder.start()
-  encoder.setRepeat(0)
-  encoder.setDelay(200)
-  encoder.setQuality(1)
-  encoder.setDispose(2)
-  encoder.setTransparent(255 * 255 * 255)
 
-  frames.forEach((f) => {
-    encoder.addFrame(f.data)
+  tmp.file((err, path, fd, cleanupCallback) => {
+    const md5sum = crypto.createHash('md5');
+    encoder.createReadStream().pipe(fs.createWriteStream(path));
+    encoder.start()
+    encoder.setRepeat(0)
+    encoder.setDelay(200)
+    encoder.setQuality(1)
+    encoder.setDispose(2)
+    encoder.setTransparent(255 * 255 * 255)
+    frames.forEach((f) => {
+      encoder.addFrame(f.data)
+    });
+    encoder.finish();
+    const stream = fs.createReadStream(path)
+  
+    stream.on('data', (d) => {
+      md5sum.update(d);
+    });
+    stream.on('end', () => {
+      const d = md5sum.digest('hex');
+      fs.rename(path, `./public/${d}.gif`, () => {
+        response.redirect(`${d}.gif`);
+      });
+    });
   });
 
-  encoder.finish()
-
-  response.setHeader('Content-Type', 'image/gif');
-  response.end(encoder.out.getData(), 200)
 }
 
 app.post('/save-gif', saveGif);
